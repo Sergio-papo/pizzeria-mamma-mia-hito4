374c2f981dc7aa0d726b9eb7c331b7c09ca2025f
Sergio Inostroza C. <sinostroza@gud.cl>
2026-04-19 11:49:05 -0400

    Hito 4 terminado - consumo de API con React

---
+++ README.md
@@ -0,0 +1,33 @@
+# React + Vite
+
+
+🍕 Pizzería Mamma Mía
+
+Proyecto desarrollado en React como parte del desafío de introducción.  
+Consiste en una aplicación que muestra distintas pizzas con sus ingredientes y precios.
+
+ Tecnologías utilizadas
+
+- ⚛️ React
+- ⚡ Vite
+- 🎨 CSS
+
+
+Estructura general:
+
+src/
+│── assets/          # Imágenes (pizzas y header)
+│── App.jsx          # Componente principal
+│── Navbar.jsx       # Barra de navegación
+│── Header.jsx       # Encabezado con imagen
+│── Home.jsx         # Vista principal
+│── CardPizza.jsx    # Tarjetas de pizzas
+│── Footer.jsx       # Pie de página
+│── App.css          # Estilos
+│── index.css        # Estilos globales
+
+
+Este proyecto es parte de un desafío académico y no incluye funcionalidades reales como autenticación o carrito.
+
+HITO 2:
+Se agrega el register y el login
---
+++ src/Home.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+import Header from './Header'
+import CardPizza from './CardPizza'
+
+const Home = () => {
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    const obtenerPizzas = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/pizzas')
+        const data = await res.json()
+        console.log(data)
+        setPizzas(data)
+      } catch (error) {
+        console.error('Error al obtener pizzas:', error)
+      }
+    }
+
+    obtenerPizzas()
+  }, [])
+
+  return (
+    <>
+      <Header />
+
+      <main className="cards-container">
+        {pizzas.map((pizza) => (
+          <CardPizza
+            key={pizza.id}
+            name={pizza.name}
+            price={pizza.price}
+            ingredients={pizza.ingredients}
+            img={pizza.img}
+          />
+        ))}
+      </main>
+    </>
+  )
+}
+
+export default Home
